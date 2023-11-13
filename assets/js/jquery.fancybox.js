3ed795aa13e9ee5c64cba6593c39d7ecdf32a27a
mycodehan <mycodehan@gmail.com>
2023-11-13 07:43:05 -0600

    content updates

--- assets/js/jquery.fancybox.js
+++ assets/js/jquery.fancybox.js
@@ -4911,9 +4911,9 @@
 				'<div class="fancybox-share">' +
 					'<h1>{{SHARE}}</h1>' +
 					'<p class="fancybox-share__links">' +
-						'<a class="fancybox-share__button fancybox-share__button--fb" href="https://www.facebook.com/sharer/sharer.php?u={{url}}">' +
+						'<a class="fancybox-share__button fancybox-share__button--fb" href="https://www.instagram.com/sharer/sharer.php?u={{url}}">' +
 							'<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m287 456v-299c0-21 6-35 35-35h38v-63c-7-1-29-3-55-3-54 0-91 33-91 94v306m143-254h-205v72h196" /></svg>' +
-							'<span>Facebook</span>' +
+							'<span>instagram</span>' +
 						'</a>' +
 						'<a class="fancybox-share__button fancybox-share__button--pt" href="https://www.pinterest.com/pin/create/button/?url={{url}}&description={{descr}}&media={{media}}">' +
 							'<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m265 56c-109 0-164 78-164 144 0 39 15 74 47 87 5 2 10 0 12-5l4-19c2-6 1-8-3-13-9-11-15-25-15-45 0-58 43-110 113-110 62 0 96 38 96 88 0 67-30 122-73 122-24 0-42-19-36-44 6-29 20-60 20-81 0-19-10-35-31-35-25 0-44 26-44 60 0 21 7 36 7 36l-30 125c-8 37-1 83 0 87 0 3 4 4 5 2 2-3 32-39 42-75l16-64c8 16 31 29 56 29 74 0 124-67 124-157 0-69-58-132-146-132z" fill="#fff"/></svg>' +
